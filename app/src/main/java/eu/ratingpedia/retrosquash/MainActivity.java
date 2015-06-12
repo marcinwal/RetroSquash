07d6609a6fa327bab927be94dfcc9ef6a4ad0d78
marcinwal <marcin.walendzik@gmail.com>
2015-06-12 23:03:48 +0100

    adding image, bitmap, canvas and drawings

--- app/src/main/java/eu/ratingpedia/retrosquash/MainActivity.java
+++ app/src/main/java/eu/ratingpedia/retrosquash/MainActivity.java
@@ -1,8 +1,12 @@
 package eu.ratingpedia.retrosquash;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
-
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +15,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ourFrame = (ImageView)findViewById(R.id.imageView);
+        Bitmap ourBitmap = Bitmap.createBitmap(300,600, Bitmap.Config.ARGB_8888);
+        Canvas ourCanvas = new Canvas(ourBitmap);
+
+        Paint paint = new Paint();
+
+        ourCanvas.drawColor(Color.BLACK);
+        paint.setColor(Color.argb(255,255,255,255));
+
+        ourCanvas.drawText("Score: 42 Lives:3 Hi: 97",10,10,paint);
+        ourCanvas.drawLine(10,50,200,50,paint);
+        ourCanvas.drawCircle(110,160,100,paint);
+        ourCanvas.drawPoint(1,260,paint);
+
+        ourFrame.setImageBitmap(ourBitmap);
+
+
     }
 
 
