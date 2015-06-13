81cd7b8159467c316965e86433004815034f35df
marcinwal <marcin.walendzik@gmail.com>
2015-06-13 11:25:44 +0100

    defining ball reflection of left/right band

--- app/src/main/java/eu/ratingpedia/retrosquash/MainActivity.java
+++ app/src/main/java/eu/ratingpedia/retrosquash/MainActivity.java
@@ -20,6 +20,8 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
+
 import java.io.IOException;
 import java.util.Random;
 
@@ -56,8 +58,8 @@ public class MainActivity extends Activity {
     boolean isBallIsMovingDown;
 
     //rocket move
-    boolean rocketIsMovingLeft;
-    boolean isRocketIsMovingRight;
+    boolean racketIsMovingLeft;
+    boolean racketIsMovingRight;
 
     //stats
     long lastFrameTime;
@@ -71,26 +73,119 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        squashCourtView = new SquashCourtView(this);
+        setContentView(squashCourtView);
 
-        ImageView ourFrame = (ImageView)findViewById(R.id.imageView);
-        Bitmap ourBitmap = Bitmap.createBitmap(300,600, Bitmap.Config.ARGB_8888);
-        Canvas ourCanvas = new Canvas(ourBitmap);
+        //sound
+        soundPool = new SoundPool(10,AudioManager.STREAM_MUSIC,0);
+        try{
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor;
 
-        Paint paint = new Paint();
+            descriptor = assetManager.openFd("sample1.ogg");
+            sample1 = soundPool.load(descriptor,0);
 
-        ourCanvas.drawColor(Color.BLACK);
-        paint.setColor(Color.argb(255,255,255,255));
+            descriptor = assetManager.openFd("sample2.ogg");
+            sample2 = soundPool.load(descriptor,0);
 
-        ourCanvas.drawText("Score: 42 Lives:3 Hi: 97",10,10,paint);
-        ourCanvas.drawLine(10,50,200,50,paint);
-        ourCanvas.drawCircle(110,160,100,paint);
-        ourCanvas.drawPoint(1,260,paint);
+            descriptor = assetManager.openFd("sample3.ogg");
+            sample3 = soundPool.load(descriptor,0);
 
-        ourFrame.setImageBitmap(ourBitmap);
+            descriptor = assetManager.openFd("sample4.ogg");
+            sample4 = soundPool.load(descriptor,0);
+
+
+        }catch (IOException e){
+            Toast.makeText(getApplicationContext(),"MUSIC FILES PROBLEM",Toast.LENGTH_LONG).show();
+        }
+
+        display = getWindowManager().getDefaultDisplay();
+        size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        //game objects
+        racketPosition = new Point();
+        racketPosition.x = screenWidth / 2;
+        racketPosition.y = screenHeight - 20;
+        racketWidth = screenWidth / 8;
+        racketHeigth = 10;
+
+        ballWidth = screenWidth / 35;
+        ballPosition = new Point();
+        ballPosition.x = screenWidth / 2;
+        ballPosition.y = 1 + ballWidth;
+
+        lives = 3;
 
 
     }
 
 
+    private class SquashCourtView extends SurfaceView implements Runnable{
+        Thread ourThread = null;
+        SurfaceHolder ourHolder;
+        volatile boolean playingSquash;
+        Paint paint;
+
+        public SquashCourtView(Context context) {
+            super(context);
+            ourHolder = getHolder();
+            paint = new Paint();
+            isBallIsMovingDown = true;
+
+            //ball sent randomly
+            Random randInt = new Random();
+            int ballDirection = randInt.nextInt(3);
+            switch(ballDirection){
+                case 0:
+                    ballIsMovingLeft = true;
+                    ballIsMovingRight = false;
+                    break;
+                case 1:
+                    ballIsMovingLeft = false;
+                    ballIsMovingRight = true;
+                    break;
+                case 2:
+                    ballIsMovingLeft = false;
+                    ballIsMovingRight = false;
+                    break;
+            }
+        }
+
+        @Override
+        public void run() {
+            while(playingSquash){
+                updateCourt();
+                drawCourt();
+                controlFPS();
+            }
+        }
+
+        private void updateCourt() {
+            if(racketIsMovingRight){
+                racketPosition.x = racketPosition.x + 10;
+            }
+
+            if(racketIsMovingLeft){
+                racketPosition.x = racketPosition.x -10;
+            }
+            //detection collision
+
+            //hit right of the screen
+            if(ballPosition.x + ballWidth > screenWidth){
+                ballIsMovingLeft = true;
+                ballIsMovingRight = false;
+                soundPool.play(sample1,1,1,0,0,1);
+            }
+
+            //hit on the left
+            if(ballPosition.x < 0){
+                ballIsMovingLeft = false;
+                ballIsMovingRight = true;
+                soundPool.play(sample1,1,1,0,0,1);
+            }
+        }
+    }
 }
